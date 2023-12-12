1fb1f83b367e5e5bc7d0e264db694848c6a1c946
nonso01 <chukwunonsomartin01@gmail.com>
2023-12-12 23:32:17 +0100

    deploy

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,4 +1,7 @@
 import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+import { useEffect } from "react"
+import { AlertTriangle } from "lucide-react"
+import anime from "animejs/lib/anime.es.js";
 
 import Text from "./Text.jsx";
 
@@ -6,13 +9,28 @@ export default function Error({}) {
   const error = useRouteError();
   console.warn(error);
 
+  useEffect(() => {
+    (function a() {
+      anime({
+        targets: ".lucide-alert-triangle path",
+        direction: "alternate",
+        delay(el, i) {
+          return i + .8 * 1e3
+        },
+        strokeDashoffset: [anime.setDashoffset, 0],
+        complete: a
+      })
+    })()
+
+  }, [])
+
   if (isRouteErrorResponse(error)) {
     return (
-      <div className="error-layout fx fx-col fx-cn fx-even pos-abs pos-cn rad-2x trans">
+      <div className="error-layout fx fx-col fx-cn fx-even pos-abs pos-cn trans">
+      <AlertTriangle stroke="#ff5a5a" />
         <h1 className="title txt-fmlg txt-danger"> {error.status}</h1>
-        <Text p={true} className="txt-danger" text={error.statusText} />
         <Text c={true} className="txt-fsm txt-lenvmap" text={error.data} />
       </div>
     );
-  } else return <h1 className="txt"> An Error occured! </h1>;
+  } else return <h1 className="title txt-danger"> Error!</h1>;
 }
