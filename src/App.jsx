34f44fd6657e80e1ab4e2f572aaef40ec618657b
nonso01 <chukwunonsomartin01@gmail.com>
2023-12-23 19:13:22 +0100

    always make use of useEffect for side effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import Text from "./components/Text.jsx";
 import Nav from "./components/Nav.jsx";
 import Button from "./components/Button.jsx";
-import H3d from "./components/Header3d.jsx";
+// import H3d from "./components/Header3d.jsx";
 import { on } from "./components/util.js";
 //
 
@@ -13,9 +13,10 @@ const minBatteryLevel = 16 / 100;
 
 export default function App() {
   // log(navigator)
-  let [themeOne, setThemeOne] = useState(true);
-  let [themeTwo, setThemeTwo] = useState(false);
-  let [themeThree, setThemeThree] = useState(false);
+  let [theme, setTheme] = useState({
+    light: true,
+    dark: false,
+  });
 
   let [mobileMedia, setMobileMedia] = useState(false);
 
@@ -27,49 +28,46 @@ export default function App() {
 
   let [isOnline, setIsOnline] = useState(true);
 
-  const handleClickThemeOne = (e) => {
-    setThemeOne((t) => (t = true));
-    setThemeTwo((t) => (t = false));
-    setThemeThree((t) => (t = false));
-  };
-
-  const handleClickThemetwo = (e) => {
-    setThemeOne((t) => (t = false));
-    setThemeTwo((t) => (t = true));
-    setThemeThree((t) => (t = false));
-  };
-
-  const handleClickThemeThree = (e) => {
-    setThemeOne((t) => (t = false));
-    setThemeTwo((t) => (t = false));
-    setThemeThree((t) => (t = true));
-  };
-
-  const deviceBatteryInfo = navigator.getBattery().then((_battery) => {
-    setBattery({
-      batteryCharging: _battery.charging,
-      batteryFull: _battery.level > minBatteryLevel,
-      level: _battery.level,
+  const handleClickThemeLight = (e) => {
+    setTheme({
+      light: true,
+      dark: false,
     });
+  };
 
-    _battery.onchargingchange = (c) => {
+  const handleClickThemeDark = (e) => {
+    setTheme({
+      dark: true,
+      light: false,
+    });
+  };
+
+  const userBatteryInfo = useEffect(() => {
+    navigator.getBattery().then((b /* BatteryManager */) => {
       setBattery({
-        batteryCharging: c.target.charging,
-        ...battery,
+        batteryCharging: b.charging,
+        batteryFull: b.level > minBatteryLevel,
+        level: b.level,
       });
-      log(c.target.charging);
-    };
 
-    _battery.onlevelchange = (l) => {
-      setBattery({
-        level: l.target.level,
-        batteryFull: l.target.level > minBatteryLevel,
-        ...battery,
-      });
-      log(l.target.level);
-    };
-  });
+      log(b);
 
+      b.onchargingchange = (c) => {
+        setBattery({
+          ...battery,
+          batteryCharging: c.target.charging,
+        });
+      };
+
+      b.onlevelchange = (l) => {
+        setBattery({
+          ...battery,
+          level: l.target.level,
+          batteryFull: l.target.level > minBatteryLevel,
+        });
+      };
+    });
+  }, []);
   const windowEvents = on(window, {
     online() {
       setIsOnline((o) => (o = true));
